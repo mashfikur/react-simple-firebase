c28c5ed0caca5d2bc9e25f0a139acc9c790d87a7
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-27 22:23:33 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Simple Firebase
+
+This is simple basic react application which is connected and configured with firebase .This project is made to learn the implimentation and usage of **Firebase**.
+
+
+
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8mvKqLFIR1Fj4iXaIOexrFWGhG7WHFAs",
+  authDomain: "react-simple-firebase-8f4c5.firebaseapp.com",
+  projectId: "react-simple-firebase-8f4c5",
+  storageBucket: "react-simple-firebase-8f4c5.appspot.com",
+  messagingSenderId: "684516364106",
+  appId: "1:684516364106:web:c4209b5de3ab68a2087bb3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
