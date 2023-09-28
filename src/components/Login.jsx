0f71c0cd1a11413fe833f2d9451a66ad57ee0bb3
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-28 12:45:00 +0600

    google signin and singout functionality added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,63 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../firebase/firebase.init";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const Login = () => {
-  const auth = getAuth(app);
+  const [user, setUser] = useState();
 
+  const auth = getAuth(app);
+  console.log(app);
   const provider = new GoogleAuthProvider();
 
-  const handleGoogleSignIn=() => {
-    signInWithPopup(auth,provider)
-    .then(result =>{
-        const user=result.user
-        console.log(user)
-    } )
-    .catch(error=>{
-        console.log("error",error.message)
-    })
-  }
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const signedInUser = result.user;
+        toast.success("Logged in Successfully");
+        console.log(signedInUser);
+        setUser(signedInUser);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+  };
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+        toast.error("User Logged Out");
+      })
+      .error((error) => {
+        console.log("error", error.message);
+      });
+  };
 
   return (
     <div>
       <h3>Please Log In</h3>
-      <button onClick={handleGoogleSignIn} >Google Login</button>
+      {user ? (
+        <button onClick={handleGoogleSignOut}> Logout</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}> Login</button>
+      )}
+
+      {user && (
+        <div>
+          <h2>User : {user.displayName} </h2>
+          <p>email :{user.email} </p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
+
+      <Toaster></Toaster>
     </div>
   );
 };
